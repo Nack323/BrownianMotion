0b261e0bd2f39746c4ec8420aca04e40a29a8927
Jose Palacios <jpalacios117@gmail.com>
2024-03-16 12:49:47 -0500

    ll

--- Prototype/main.py
+++ Prototype/main.py
@@ -1,5 +1,6 @@
 from particle import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 """ Description of experiment
 there will be n particles of the same size and all will have
@@ -7,7 +8,74 @@ a momentum and radius. The colission between them will be completely
 elastic and they will bounce against walls
  """
 
-n = 10
+n = 100
+steps = 500
+DT = 0.01
+massMultiplier = 1 # this will be multiplied to the radious cubed so equal density
+                   # between all particles is assumed
+                   
+# create particles
+particleArray = [Particle(0.1, np.random.rand() * 5 + 2.5, np.random.rand() * 5 + 2.5, 
+    np.random.rand() * 10 - 5, np.random.rand() * 10 - 5, DT) for i in range(n)]
 
-particleArray = [Particle(0.1, np.random(), np.random(), 
-    np.random(), np.random(), 0.01)]
+def particleDistance(a, b):
+    xd = a.x - b.x
+    yd = a.y - b.y
+    return np.linalg.norm([xd, yd])
+
+# create update loop
+for i in range(steps):
+    # step all particles
+    for p in particleArray:
+        p.step()
+    
+    # check for colission
+    for a in range(n):
+        for b in range(n):
+            if not a == b:
+                minDist = particleArray[a].r + particleArray[b].r
+                dist = particleDistance(particleArray[a], particleArray[b])
+                if dist <= minDist:
+                    #calculate new velocities
+                    particleArray[a].unstep()
+                    particleArray[b].unstep()
+                    va = particleArray[a].getVVector()
+                    vb = particleArray[b].getVVector()
+                    ma = particleArray[a].r ** 3 * massMultiplier
+                    mb = particleArray[b].r ** 3 * massMultiplier
+                    ra = particleArray[a].pvector
+                    rb = particleArray[b].pvector
+                    M = ma + mb
+                    ua = va - 2 * mb / M * (np.dot((va - vb), (ra - rb)))/(np.linalg.norm(ra - rb) ** 2) * (ra - rb)
+                    ub = vb - 2 * ma / M * (np.dot((vb - va), (rb - ra)))/(np.linalg.norm(rb - ra) ** 2) * (rb - ra)
+                    particleArray[a].setVVector(ua)
+                    particleArray[b].setVVector(ub)
+                    print('aa')
+    for particle in particleArray:
+        if particle.x >= 9.9 or particle.x <=0.1:
+            vx = particle.vx
+            vy = particle.vy
+            particle.setVVector([-vx, vy])
+        if particle.y >= 9.9 or particle.y <=0.1:
+            vx = particle.vx
+            vy = particle.vy
+            particle.setVVector([vx, -vy])
+    # render and save frame
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.set_xlim([0, 10])
+    ax.set_ylim([0, 10])
+    circles = [
+        plt.Circle(
+            particleArray[i].getPVector(),
+            particleArray[i].r
+        )
+        for i in range(n)
+    ]
+    
+    for c in circles:
+        ax.add_patch(c)
+    fig.savefig(f"frames/frame{str(i).zfill(4)}.png")
+    plt.close(fig)
+
+import os
+os.system("cd frames; rm *; ffmpeg -framerate 30 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p out.mp4")
--- Prototype/particle.py
+++ Prototype/particle.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Particle:
 
@@ -8,13 +9,32 @@ class Particle:
         self.vy = vy
         self.dt = dt
         self.r = r
+        self.pvector = np.array([self.x, self.y])
+        self.vvector = np.array([self.vx, self.vy])
 
     def step(self):
         self.x = self.x + self.dt * self.vx 
         self.y = self.y + self.dt * self.vy 
+        self.pvector = np.array([self.x, self.y])
+        self.vvector = np.array([self.vx, self.vy])
 
     def unstep(self): # probably unnecessary
         self.x = self.x - self.dt * self.vx 
+        self.y = self.y - self.dt * self.vy 
+        self.pvector = np.array([self.x, self.y])
+        self.vvector = np.array([self.vx, self.vy])
 
     def getPos(self):
         return self.x, self.y
+    
+    def getPVector(self):
+        return self.pvector
+    
+    def getVVector(self):
+        return self.vvector
+    
+    def setVVector(self, v):
+        self.vx = v[0]
+        self.vy = v[1]
+        self.pvector = np.array([self.x, self.y])
+        self.vvector = np.array([self.vx, self.vy])
